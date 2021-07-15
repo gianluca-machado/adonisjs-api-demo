75d70e50573b07801ef6ebeb700d448e47b6ff41
Gianluca Maziero Machado <gianluca.machado@opus-software.com.br>
2021-07-15 12:00:08 -0300

    Update cors values

--- config/cors.js
+++ config/cors.js
@@ -16,7 +16,10 @@ module.exports = {
   | Function - Receives the current origin and should return one of the above values.
   |
   */
-  origin: false,
+  origin: (currentOrigin) => {
+    const allowedOrigins = ['*'];
+    return allowedOrigins.includes('*') || allowedOrigins.includes(currentOrigin);
+  },
 
   /*
   |--------------------------------------------------------------------------
